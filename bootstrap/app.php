4645a943433d185dee1cc094104f1d773ed9b3f1
Sita Kumari Barahee <sitakumaribarahee@gmail.com>
2026-06-27 19:04:17 +0545

    fix: update welcome message and adjust formatting in app configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,14 +8,13 @@ use Illuminate\Http\Request;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
-        commands: __DIR__ . '/../routes/console.php',
         api: __DIR__ . '/../routes/api.php',
-
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(static function (Middleware $middleware): void {})
     ->withExceptions(static function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
-            static fn(Request $request) => $request->is('api/*'),
+            static fn (Request $request) => $request->is('api/*'),
         );
     })->create();
